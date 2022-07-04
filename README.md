4c3851262238fb8f3de36335c710d0436bd9c8d6
Anastasiia Melnyk <nastaisha1988@gmail.com>
2022-07-05 01:25:40 +0300

    HW-8

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# goit-markup-hw-08
-HW8
+# goit-markup-hw-06
+HW6
---
+++ js/menu.js
@@ -0,0 +1,16 @@
+(() => {
+  const refs = {
+    openMenuBtn: document.querySelector('.menu-btn'),
+    closeMenuBtn: document.querySelector('.mob-menu__btn'),
+    menu: document.querySelector('.mob-menu'),
+    body: document.querySelector('body'),
+  };
+
+  refs.openMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuBtn.addEventListener('click', toggleMenu);
+
+  function toggleMenu() {
+    refs.menu.classList.toggle('is-hidden');
+    refs.body.classList.toggle('no-scroll');
+  }
+})();
